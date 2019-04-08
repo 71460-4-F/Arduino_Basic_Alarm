487aff437465a0b17283eba1b61685d4d12f3697
Tiago A. Fontenele <38273600+71460-4-F@users.noreply.github.com>
2019-04-08 20:56:48 -0300

    Create code.ino

---
+++ code.ino
@@ -0,0 +1,32 @@
+//(Alarme Residencial)
+
+int analogPin = A1;
+int relePin = 8;   
+int ledPin = 2;
+int tempoLigado = 10; 
+int val = 0; 
+ 
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(relePin, OUTPUT);
+  pinMode(ledPin, OUTPUT);
+  pinMode(analogPin, INPUT);
+}
+ 
+void loop(){
+  val = analogRead(analogPin);
+  if(val > 100){
+    digitalWrite(relePin,HIGH);
+    for(int i = 0; i < tempoLigado; i++){ 
+      digitalWrite(ledPin, HIGH);
+      delay(1000); // espera 1 seg
+      digitalWrite(ledPin, LOW);
+      delay(500);
+    } // fim do laço for
+  }
+  else
+  {
+     digitalWrite(relePin,LOW); 
+  }
+}
